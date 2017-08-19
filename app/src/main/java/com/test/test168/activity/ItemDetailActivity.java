8616a9df026470f165c2d703ca1faa255714de29
xian <me@qq.com>
2017-08-19 11:03:56 +0800

    add config.gradle

--- app/src/main/java/com/test/test168/activity/ItemDetailActivity.java
+++ app/src/main/java/com/test/test168/activity/ItemDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
@@ -21,7 +22,6 @@ import com.test.test168.fragment.ItemDetailFragment;
  * in a {@link ItemListActivity}.
  */
 public class ItemDetailActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/test/java/com/test/test168/RxjavaTest.java
@@ -0,0 +1,12 @@
+package com.test.test168;
+
+/**
+ * Created by xian on 2017/8/19.
+ */
+
+public class RxjavaTest {
+    @Test
+    public void test1() throws Exception {
+
+    }
+}
